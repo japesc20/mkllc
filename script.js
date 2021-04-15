8bdfdc5a99639d68ccc05787b8446ea859607d24
Jacob Petrakovitz <73858615+japesc20@users.noreply.github.com>
2021-04-15 18:23:54 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
-const navLinks = document.getElementById('nav-link');
+const navLinks = document.querySelectorAll('.nav-item');
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
 
-navLinks.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', () => {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.toggle('active');
+    })
 })
